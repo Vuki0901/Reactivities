420c9dc56875b6181f55569a2ae08af8268fbaa2
Vuki0901 <leo.vukoje02@gmail.com>
2021-10-16 14:13:58 +0200

    error validation

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,5 +12,14 @@ namespace API.Controllers
         private IMediator _mediator;
         protected IMediator Mediator => 
             _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T> (Result<T> r)
+        {
+            if(r.IsSuccess && r.Value != null)
+                return Ok(r.Value);
+            if(r.IsSuccess && r.Value == null || r == null)
+                return NotFound();
+            return BadRequest(r.Error);
+        }
     }
 }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,11 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Route, useLocation } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 import { Container } from 'semantic-ui-react';
+import NotFound from '../../errors/NotFound';
+import ServerError from '../../errors/ServerError';
+import TestErrors from '../../errors/TestError';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import ActivityForm from '../../features/activities/form/ActivityForm';
@@ -15,6 +19,7 @@ function App() {
 
   return(
     <>
+      <ToastContainer position='bottom-right' hideProgressBar/>
       <Route exact path="/" component={HomePage}/>
       <Route 
         path={"/(.+)"}
@@ -22,9 +27,14 @@ function App() {
           <>
           <NavBar/>
           <Container>
-            <Route exact path="/activities" component={ActivityDashboard}/>
-            <Route path="/activities/:id" component={ActivityDetails}/>
-            <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm}/>
+            <Switch>
+              <Route exact path="/activities" component={ActivityDashboard}/>
+              <Route path="/activities/:id" component={ActivityDetails}/>
+              <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm}/>
+              <Route path="/errors" component={TestErrors}/>
+              <Route path="/server-error" component={ServerError}/>
+              <Route component={NotFound}/>
+            </Switch>
           </Container>
           </>
         )}
